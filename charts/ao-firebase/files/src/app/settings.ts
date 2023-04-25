1f307641d35995eb7f107f064476f54cfb102b92
gornication <49779801+gornication@users.noreply.github.com>
2023-04-25 19:55:27 +0700

    Delete ao-firebase

--- charts/ao-firebase/files/src/app/settings.ts
+++
@@ -1,6 +0,0 @@
-export default {
-  type: 'service_account',
-  projectId: process.env.FIREBASE_PROJECT_ID ?? '',
-  privateKey: process.env.FIREBASE_PRIVATE_KEY ?? '',
-  clientEmail: process.env.FIREBASE_CLIENT_EMAIL ?? ''
-};
--- charts/ao-firebase/files/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { Server } from './app';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-void new Server().bootstrap();
